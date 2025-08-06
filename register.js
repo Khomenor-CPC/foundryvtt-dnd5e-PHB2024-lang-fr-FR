bf60db544574b39254d1963d332aa731cec6b945
Jessy GRUSON <jgruson@sii.fr>
2025-08-06 12:10:41 +0200

    premier commit

---
+++ register.js
@@ -0,0 +1,25 @@
+const MODULE_ID = 'dnd5e_PHB2024_fr-FR'; // Change this ID!
+
+// No need to change the code below this line, but it’s your module so do it if you want!
+
+Hooks.on('init', () => {
+  game.settings.register(MODULE_ID, 'autoRegisterBabel', {
+    name: 'Automatically activate translation via Babele',
+    hint: 'Automatically implements Babele translations without needing to point to the directory containing the translations.',
+    scope: 'world',
+    config: true,
+    default: true,
+    type: Boolean,
+    requiresReload: true,
+  });
+
+  if (game.settings.get(MODULE_ID, 'autoRegisterBabel')) {
+    Hooks.once('babele.init', (babele) => {
+      babele.register({
+        module: MODULE_ID,
+        lang: 'fr',
+        dir: 'compendium/fr',
+      });
+    });
+  }
+});
